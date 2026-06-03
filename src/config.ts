e459f4b9162493f6512e7ce8de1f52428a307b2b
bulat <galimyanovb@gmail.com>
2026-06-03 20:49:38 +0300

    Init

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export default {
+    PORT: Number(process.env.PORT),
+
+    DB_PORT: Number(process.env.DB_PORT),
+    DB_USERNAME: process.env.DB_USERNAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_NAME: process.env.DB_NAME,
+    DB_HOST: process.env.DB_HOST,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
+import 'dotenv/config';
+
 import express from 'express';
+
+import config from './config';
+import { AppDataSource } from './data-source';
 
 const app = express();
 
-const port = 5000;
+void (async function() {
+    await AppDataSource.initialize();
+    console.log('DB is here!');
 
-app.listen(port, () => {
-  console.log(`Server started on ${String(port)}`);
-});
+    app.listen(config.PORT, () => {
+        console.log(`Server started on ${String(config.PORT)}`);
+    });
+})();
